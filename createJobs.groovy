032c1cda05d4d88accff7d364a3df2a90e8cd816
Mai Gia Truong <maigia.truongzuhlke.com@Mais-MacBook-Pro.local>
2021-04-16 23:07:57 +0800

    change job

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/truongmg/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
